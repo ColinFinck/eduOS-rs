aac84193a362c918d6f00046c60c58deffdba512
Stefan Lankes <stlankes@users.noreply.github.com>
2017-10-29 23:46:38 +0100

    remove typos

--- src/scheduler/task.rs
+++ src/scheduler/task.rs
@@ -114,19 +114,20 @@ pub trait TaskFrame {
 
 impl Drop for Task {
     fn drop(&mut self) {
-		debug!("dealloactae stack of task {} (stack at 0x{:x})", self.id, self.stack as usize);
+	debug!("deallocate stack of task {} (stack at 0x{:x})", self.id, self.stack as usize);
 
-		// deallocate stack
-        unsafe {
-			Heap.dealloc(self.stack as *mut u8, Layout::new::<KernelStack>());
-		}
+	// deallocate stack
+	unsafe {
+		Heap.dealloc(self.stack as *mut u8, Layout::new::<KernelStack>());
+	}
     }
 }
 
 impl Task {
 	pub fn new(tid: TaskId, task_status: TaskStatus) -> Task {
 		let tmp = unsafe { Heap.alloc(Layout::new::<KernelStack>()).unwrap() as *mut KernelStack };
-		debug!("alloactae stack for task {} at 0x{:x}", tid, tmp as usize);
+
+		debug!("allocate stack for task {} at 0x{:x}", tid, tmp as usize);
 
 		Task {
 			id: tid,
